31d6eeac6c42f4bd6e5c508a866b93a6c0b4ec46
ravi <ravik9158@hotmail.com>
2021-05-25 20:18:00 +0530

    Adding initial files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Welcome to confusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
