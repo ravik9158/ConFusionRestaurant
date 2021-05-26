fdaa572fb34bf351bb6fd11339e776d001a9874d
ravi <ravik9158@hotmail.com>
2021-05-26 20:55:11 +0530

    adding new Js file to store data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div>
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Welcome to confusion</NavbarBrand>
+            <NavbarBrand href="/">Contemporay-Fusion Restaurant</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
